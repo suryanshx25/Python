9566812e1700293cbe03ad463a5d83594761e798
Suryansh Singh <suryanshx25@gmail.com>
2024-11-16 10:59:32 +0530

    1

--- class.4.py
+++ class.4.py
@@ -1,2 +1,21 @@
 # Strings
 
+name = "suryansh"
+friend = "chirag"
+print("Hello, " + name)
+
+apple = '''He said,
+Hi Suryansh
+hey I am good
+I want to eat an apple'''
+
+print(apple)
+
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+
+for character in apple:
+    print(character)
